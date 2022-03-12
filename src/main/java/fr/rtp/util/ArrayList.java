892a3360c5de8f5081e498669e6add9d3307340e
theray1 <erwanbd@outlook.fr>
2022-03-12 18:23:33 +0100

    Replace Nested Conditional with Guard Clause

--- src/main/java/fr/rtp/util/ArrayList.java
+++ src/main/java/fr/rtp/util/ArrayList.java
@@ -6,21 +6,23 @@ public class ArrayList {
     private int size = 0;
 
     public void add(Object child) {
-        // <1>
-        if (!readOnly) {
-            int newSize = size + 1;
-        // <2>
-            if (newSize > elements.length) {
-                Object[] newElements = new Object[elements.length + 10];
-                for (int i = 0; i < size; i++) {
-                    newElements[i] = elements[i];
-                }
-                elements = newElements;
-            }
-        // <3>
-            elements[size] = child;
-            size++;
+
+        if (readOnly) {
+            return;
         }
+
+        int newSize = size + 1;
+
+        if (newSize > elements.length) {
+            Object[] newElements = new Object[elements.length + 10];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+
+        elements[size] = child;
+        size++;
     }
 
     public void setReadOnly(boolean readOnly) {
